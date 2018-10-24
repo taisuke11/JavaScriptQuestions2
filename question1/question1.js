9150eae000dab111c516afa82c0f7ea8c72981ec
taisuke11 <r.taisuke.2018@gmail.com>
2018-10-24 09:48:26 +0900

    Finshed question1

--- question1/question1.js
+++ question1/question1.js
@@ -1,2 +1,13 @@
 'use strict';
-console.log ('questison1');
+var emptyBottles = 0;
+var drinks = 100;
+console.log ('購入した本数:' + String (drinks));
+emptyBottles = drinks;
+//空き瓶が2本以下になり交換できなくなったら、ループを終了する。
+do {
+  var addDrinks = 0;
+  addDrinks = Math.floor (emptyBottles / 3);
+  drinks += addDrinks;
+  emptyBottles = emptyBottles % 3 + addDrinks;
+} while (emptyBottles > 2);
+console.log ('飲める本数:' + String (drinks));
