008c60a7ca092da38ee99d0bdee323fee41aa780
taisuke11 <r.taisuke.2018@gmail.com>
2018-10-26 12:01:51 +0900

    Refactoring Question3

--- question3/question3.js
+++ question3/question3.js
@@ -1,14 +1,19 @@
 'use strict';
 console.log ('questison3');
 //お店の情報
+//在庫管理なので、意味合い的に考えて
+//空き瓶の変数は敢えてStoreの方で含んでいません。
 class Store {
-  constructor (stockDrink, serviceConditionNumber, price) {
+  constructor (shopName, stockDrink, serviceConditionNumber, price) {
     this.stockDrink = stockDrink;
     this.serviceConditionNumber = serviceConditionNumber;
     this.price = price;
+    this.shopName = shopName;
   }
-  //売った本数(在庫切れの可能性がある為)と合計金額(この際に在庫を減らし、買えた本数を返す。金額は表示だけ)
-  SelledDrinks (wantBuyDrinks) {
+
+  //幾つジュースを買うかを求める。この際に在庫を減らす。
+  //また、在庫によっては空瓶(＝買えた数)が足りない時がある。
+  EmptyBottlesByBuyDrinks (wantBuyDrinks, emptyBottles) {
     var buyDrinks = wantBuyDrinks;
     if (this.stockDrink < wantBuyDrinks) {
       buyDrinks = this.stockDrink;
@@ -16,13 +21,17 @@ class Store {
     } else {
       this.stockDrink -= buyDrinks;
     }
+    console.log (this.shopName);
     console.log ('購入希望本数：' + String (wantBuyDrinks) + '本');
     console.log ('買えた本数：' + String (buyDrinks) + '本');
     console.log ('金額：' + String (buyDrinks * this.price) + '円');
-    return buyDrinks;
+    emptyBottles += buyDrinks;
+    return emptyBottles;
   }
-  //サービスドリンクによる空瓶の数を求める(この際に在庫を減らし、空瓶の数を返す)
-  NumberOfEmptyBottlesByServiceDrink (emptyBottles) {
+
+  //サービスによる空瓶の数を求める。この際に在庫を減らす。
+  //また、在庫によってはサービスできない場合もある
+  EmptyBottlesByServiceDrinks (emptyBottles) {
     var serviceDrinks = 0;
     if (emptyBottles >= this.serviceConditionNumber) {
       serviceDrinks = Math.floor (emptyBottles / this.serviceConditionNumber);
@@ -38,27 +47,39 @@ class Store {
     console.log ('現在の在庫：' + String (this.stockDrink) + '本');
     return emptyBottles;
   }
+
+  //空き瓶によるジュースをもらえるサービスが出来るかどうかの判定
+  //サービスによって手に入った空き瓶(ジュース)を使って
+  //更に空き瓶(ジュース)を手に入れる事を判定するために用意した関数
+  CanWeService (emptyBottles) {
+    if (emptyBottles < this.serviceConditionNumber) {
+      return false;
+    }
+    return true;
+  }
 }
-//買った本数の配列が必要
+
 let wantBuyDrinks = [1, 3, 20, 2, 60, 100];
 
-//空瓶の本数
-var AStoreEmptyBottles = 0;
-var BStoreEmptyBottles = 0;
-
 //店舗ごとのお店の情報を設定する
-let AStore = new Store (100, 3, 200);
-let BStore = new Store (200, 4, 100);
+let Stores = [new Store ('A店', 100, 3, 200), new Store ('B店', 200, 4, 100)];
+var storeEmptyBottles = [0, 0];
 for (let number of wantBuyDrinks) {
-  //処理として纏める
-  console.log ('A店');
-  AStoreEmptyBottles += AStore.SelledDrinks (number);
-  AStoreEmptyBottles = AStore.NumberOfEmptyBottlesByServiceDrink (
-    AStoreEmptyBottles
-  );
-  console.log ('B店');
-  BStoreEmptyBottles += BStore.SelledDrinks (number);
-  BStoreEmptyBottles = BStore.NumberOfEmptyBottlesByServiceDrink (
-    BStoreEmptyBottles
-  );
+  for (let i of [0, 1]) {
+    storeEmptyBottles[i] = Stores[i].EmptyBottlesByBuyDrinks (
+      number,
+      storeEmptyBottles[i]
+    );
+    //サービスによって手に入った空き瓶(ジュース)を使って
+    //更に空き瓶(ジュース)を手に入れる事を想定しています。
+    while (true) {
+      storeEmptyBottles[i] = Stores[i].EmptyBottlesByServiceDrinks (
+        storeEmptyBottles[i]
+      );
+      if (!Stores[i].CanWeService (storeEmptyBottles[i])) {
+        break;
+      }
+    }
+    console.log ('空き瓶数：' + String (storeEmptyBottles[i]) + '本');
+  }
 }
